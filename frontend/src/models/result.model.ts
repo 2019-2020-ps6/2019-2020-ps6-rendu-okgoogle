c54852013493f453a198356b689e24e6dc1300ab
rachid <rachid.eladlani83@gmail.com>
2020-04-07 12:38:42 +0200

    Create Statistic branch

--- frontend/src/models/result.model.ts
+++ frontend/src/models/result.model.ts
@@ -3,15 +3,15 @@ import { Question } from './question.model';
 
 export interface Result {
   id: string;
-  userId: string; //ICI
-  quizId: string;//ICI
+  userId: string;
+  quizId: string;
   nameQuiz: string;
   dateQuiz: string;
-  nbErreur: number; // seront incrémenter au fil des reponses //Backend
-  nbCorrect: number; // seront incrémenter au fil des reponses //Backend
-  nbAide: number; // seront incrémenter au fil des reponses //ICI
-  questions: Question[], //ICI
-  answers: Answer[], //ICI
-  dateJeu: string; //ICI
-  dureeJeu: number; //Durée de jeu ICI
+  nbErreur: number; 
+  nbCorrect: number
+  nbAide: number; 
+  questions: Question[],
+  answers: Answer[],
+  dateJeu: string;
+  dureeJeu: number;
 }
