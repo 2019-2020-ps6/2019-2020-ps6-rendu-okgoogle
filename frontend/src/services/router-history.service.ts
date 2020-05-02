240d6351ca8e1e985075bda5711b54b005db0e5d
Abdel <abdelouhab.belkhiri@etu.unice.fr>
2020-05-02 17:19:59 +0200

    Historique de navigation (rooting)

--- frontend/src/services/router-history.service.ts
+++ frontend/src/services/router-history.service.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject } from 'rxjs';
 import { Injectable, OnInit } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
+import { Router, NavigationStart, NavigationError } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -15,36 +15,55 @@ export class RouterHistoryService implements OnInit {
   private previousUrl: string = undefined;
 
   constructor(private router: Router) {
-      this.router.events.subscribe(event => {
-        if (event instanceof NavigationStart ) {
-          if(event.restoredState){
-            this.navigation.pop();
-            this.navigation$.next(this.navigation);
-          }
-          else{
-            this.previousUrl = this.router.url.split('/')[1];
-            var title = ''; 
-            switch(this.previousUrl) {
-              case 'theme-list':
-                title = 'à la liste de thèmes';
-                break;
-              case 'user-list':
-                title = "à la liste d'utilisateurs";
-                break;
-              case 'quiz-list':
-                title = 'à la liste de quiz';
-                break;
-              default:
-                title = "à la page précedente";
-                break;
-            }
-            this.navigation.push(title);
-            this.navigation$.next(this.navigation);
-          }
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (event.navigationTrigger == "popstate") {
+          this.navigation.pop();
+          this.navigation$.next(this.navigation);
         }
-      });
+        else {
+          this.previousUrl = this.router.url.split('/')[1];
+          var title = '';
+          switch (this.previousUrl) {
+
+            //gestion partie accueilli (traitement alzheimer)
+            case 'theme-list':
+              title = 'à la liste de thèmes';
+              break;
+            case 'user-list':
+              title = "à la liste d'utilisateurs";
+              break;
+            case 'quiz-list':
+              title = 'à la liste de quiz';
+              break;
+
+            //Gestion partie admin
+            case 'main-admin':
+              title = "à la page d'accueil personnel santé";
+              break;
+            case 'theme-edit':
+              title = "à la page création de quiz";
+              break;
+            case 'create-theme':
+              title = "à la page création de thème";
+              break;
+            case 'user-stat':
+              title = "à la page statistiques";
+              break;
+            default:
+              title = "à la page précedente";
+              break;
+          }
+          this.navigation.push(title);
+          this.navigation$.next(this.navigation);
+        }
+      } else if (event instanceof NavigationError) {
+        // Present error to user
+        console.log(event.error);
+      }
+    });
   }
-  ngOnInit(): void {
+  ngOnInit() {
 
   }
 
