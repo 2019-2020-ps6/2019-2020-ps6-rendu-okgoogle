cd7a3128d080634723e8645a0555b902ae6761a5
Abdel <abdelouhab.belkhiri@etu.unice.fr>
2020-05-20 15:10:25 +0200

    Clean code

--- frontend/src/app/admin/gestion/edit-quiz/edit-quiz.component.ts
+++ frontend/src/app/admin/gestion/edit-quiz/edit-quiz.component.ts
@@ -15,21 +15,21 @@ export class editQuizComponent implements OnInit {
 
   public quiz: Quiz;
   public curTheme: Theme
-  public QuatreImg1question = "./QuatreImgUnequestion.jpg";
-  public UneImg4question = "./UneImgQuatreReponse.jpg";
+  public modelImageInAnswers = "./QuatreImgUnequestion.jpg";
+  public modelImageInQuestion = "./UneImgQuatreReponse.jpg";
   public quizForm: FormGroup;
-  public edit_quiz: boolean = false;
+  public quizEdit: boolean = false;
 
-  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private quizService: QuizService, private themeService: ThemeService) { 
-    
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private quizService: QuizService, private themeService: ThemeService) {
+
   }
-  
+
   ngOnInit() {
     const themeid = this.route.snapshot.paramMap.get('themeid');
     const quizid = this.route.snapshot.paramMap.get('quizid');
     this.themeService.setSelectedTheme(themeid.toString())
-    this.themeService.themeSelected$.subscribe((theme)=>this.curTheme = theme)
-    this.quizService.setSelectedQuiz(quizid.toString(),themeid.toString());
+    this.themeService.themeSelected$.subscribe((theme) => this.curTheme = theme)
+    this.quizService.setSelectedQuiz(quizid.toString(), themeid.toString());
     this.quizService.quizSelected$.subscribe((quiz) => {
       this.quiz = quiz
       this.initializeQuizForm()
@@ -40,24 +40,24 @@ export class editQuizComponent implements OnInit {
   private initializeQuizForm() {
     this.quizForm = this.formBuilder.group({
       name: this.quiz.name,
-      imageUrl:this.quiz.imageUrl
+      imageUrl: this.quiz.imageUrl
     });
   }
 
-  editQuiz(){
-    this.edit_quiz=true;
+  editQuiz() {
+    this.quizEdit = true;
   }
 
-  validateQuiz(){
+  validateQuiz() {
     const themeid = this.route.snapshot.paramMap.get('themeid');
     const quizid = this.route.snapshot.paramMap.get('quizid');
     const quiz = this.quizForm.getRawValue() as Quiz;
-    this.quizService.editQuiz(themeid,quizid,quiz)
-    this.edit_quiz = false;
+    this.quizService.editQuiz(themeid, quizid, quiz)
+    this.quizEdit = false;
   }
 
-  annulerQuiz(){
-      this.edit_quiz = false;
+  cancelQuiz() {
+    this.quizEdit = false;
   }
 
 }
